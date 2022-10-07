593abc91cca03a2c8f57ca187b2abec44ae3c992
Asitha Navaratne <asithan476@gmail.com>
2022-10-07 13:34:34 +0530

    Copying to clipboard complete

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -6,13 +6,28 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(",");
   const hex = rgbToHex(...rgb);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [alert]);
+
   return (
     <article
       className={`color ${index > 9 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex || `#${hexColor}`);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex || `#${hexColor}`}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
