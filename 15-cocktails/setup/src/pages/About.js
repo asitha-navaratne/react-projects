875fe97ce6700a36ad907da474a6b728e0630edf
Asitha Navaratne <asithan476@gmail.com>
2022-10-19 23:27:59 +0530

    About and Error pages done

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        maiores veritatis earum sint obcaecati iure molestiae expedita. Officia,
+        deleniti incidunt itaque similique doloribus aliquam ipsam maxime
+        impedit molestiae cum quas minima nesciunt ratione aut consectetur animi
+        dolorum, consequatur sit temporibus.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
