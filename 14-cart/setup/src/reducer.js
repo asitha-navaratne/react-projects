b9973c383f87c58cb4846aeec9fa7a99ccd7c956
Asitha Navaratne <asithan476@gmail.com>
2022-10-19 21:57:16 +0530

    Get cart total functionality complete

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -22,18 +22,41 @@ const reducer = (state, action) => {
   }
 
   if (action.type === "DECREASE") {
-    let tempCart = state.cart.map((item) => {
-      if (item.id === action.payload) {
-        return {
-          ...item,
-          amount: item.amount === 0 ? item.amount : item.amount - 1,
-        };
-      }
-      return item;
-    });
+    let tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return {
+            ...item,
+            amount: item.amount - 1,
+          };
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
 
     return { ...state, cart: tempCart };
   }
+
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, total, amount };
+  }
+
   return state;
 };
 
